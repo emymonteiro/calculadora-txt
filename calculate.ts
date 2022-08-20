c7e0be52be4d738568477e532113ec8279410057
Emilly <emymonteiro.sweetie@gmail.com>
2022-08-20 16:32:52 -0400

    add-config-file

--- calculate.ts
+++ calculate.ts
@@ -1,15 +1,17 @@
 const fs = require('fs')
 
+const config = require('./config.json')
+
 function filesFolder(){
-    const result = fs.readdirSync('./files/') 
+    const result = fs.readdirSync(config.filesPath) 
     return result
 }
 
 function readText(){
     let calculate = 0
-    const files = filesFolder()
+    const files = config.getCustomFiles !== "" ? config.getCustomFiles : filesFolder()
     files.forEach(el => {
-        const result = fs.readFileSync('./files/' + el, 'utf8')
+        const result = fs.readFileSync(config.getCustomFiles === "" ? config.filesPath : '' + el, 'utf8')
         calculate += Number(result)
     })
     return calculate
@@ -17,7 +19,7 @@ function readText(){
 
 function writeFile(){
     const result = readText()
-    fs.writeFileSync('result.txt', result.toString())
+    fs.writeFileSync(config.resultFile, result.toString())
 }
 
 writeFile()
